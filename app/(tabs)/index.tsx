05eb19ca0d83cb0c172b7b9e9ddf84cda2a65cba
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-09-26 14:40:44 -0400

    formatting article page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,33 @@
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import Card from '../../shared/Card';
-import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
 export default function TabOneScreen() {
+  const [articles, setArticles] = useState([
+      { title: 'article 1', author: 'author 1', content: 'content 1', key: '1' },
+      { title: 'article 2', author: 'author 2', content: 'content 2', key: '2' },
+      { title: 'article 3', author: 'author 3', content: 'content 3', key: '3' },
+      { title: 'article 4', author: 'author 4', content: 'content 4', key: '4' },
+      { title: 'article 5', author: 'author 5', content: 'content 5', key: '5' },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Card>
-      <Text style={styles.title}>Article Name</Text>
-      <Text style={styles.author}>Author Name</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text style={styles.content}>Article Content</Text>
-      </Card>
+      <FlatList
+        data={articles}
+        numColumns={2}
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <Card>
+              <Text style={styles.title}>{item.title}</Text>
+              <Text style={styles.author}>{item.author}</Text>
+              <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+              <Text style={styles.content}>{item.content}</Text>
+            </Card>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
@@ -22,6 +38,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10,
   },
   title: {
     fontSize: 20,
